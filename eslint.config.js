cfbad9d21cc0f336a6571b51bf6c23ab3c68dc8d
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-05-29 15:13:18 -0300

    eslint + prettier

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+const ignores = {
+  ignores: [
+    'coverage/**',
+    'public/**',
+    'dist/**',
+    'pnpm-lock.yaml/**',
+    'pnpm-workspace.yaml/**',
+  ],
+};
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ignores,
+  eslintPluginPrettierRecommended,
+];
