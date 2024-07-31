a88a98e7514e9b077e4e97b2964f717a62cca759
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-07-31 08:20:27 -0300

    feat(app): added Title component

--- packages/app/src/Aplication/Components/Typography/Title.tsx
+++ packages/app/src/Aplication/Components/Typography/Title.tsx
@@ -7,13 +7,6 @@ interface TitleProps {
   variant?: TVariant;
 }
 
-const headingTypes = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-};
-
 const headingClasses = {
   h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
   h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
@@ -22,8 +15,7 @@ const headingClasses = {
 };
 
 export const Title = ({ children, variant = 'h1' }: TitleProps) => {
-  const HeadingTag = headingTypes[variant];
   const headingClass = headingClasses[variant];
 
-  return React.createElement(HeadingTag, { className: headingClass }, children);
+  return React.createElement(variant, { className: headingClass }, children);
 };
