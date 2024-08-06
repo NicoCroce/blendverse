73cea19817afbd782e05156c405cc837ce117e65
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-06 16:47:08 -0300

    feat(server): added error in usecase

--- packages/server/src/data/mock.ts
+++ packages/server/src/data/mock.ts
@@ -15,4 +15,32 @@ export const Products = [
     stock: 10,
     price: 100,
   },
+  {
+    id: '1',
+    name: 'Remera 1',
+    description: 'Remera de color negro',
+    stock: 10,
+    price: 1002,
+  },
+  {
+    id: '2',
+    name: 'Remera 2',
+    description: 'Remera de color negro',
+    stock: 10,
+    price: 1003,
+  },
+  {
+    id: '3',
+    name: 'Remera 3',
+    description: 'Remera de color negro',
+    stock: 10,
+    price: 103,
+  },
+  {
+    id: '4',
+    name: 'Remera 4',
+    description: 'Remera de color negro',
+    stock: 10,
+    price: 104,
+  },
 ];
--- packages/server/src/domains/Products/Domain/UseCases/GetProduct.usecase.ts
+++ packages/server/src/domains/Products/Domain/UseCases/GetProduct.usecase.ts
@@ -1,4 +1,4 @@
-import { IUseCase } from '@server/Application';
+import { AppError, IUseCase } from '@server/Application';
 import { ProductsRepository } from '../Products.repository';
 import { Product } from '../Product.entity';
 
@@ -6,6 +6,8 @@ export class GetProduct implements IUseCase<Product | null> {
   constructor(private readonly productsRepository: ProductsRepository) {}
 
   async execute(id: string): Promise<Product | null> {
-    return (await this.productsRepository.getProduct(id)) || null;
+    const product = await this.productsRepository.getProduct(id);
+    if (!product) throw new AppError('Producto no encontrado', 404);
+    return product;
   }
 }
