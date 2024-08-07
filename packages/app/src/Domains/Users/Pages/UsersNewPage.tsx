fbe02937a5a5baa4a334ddce0f3998da6f825c8d
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-07 10:30:08 -0300

    feat(app): added Card to form of NewUser

--- packages/app/src/Domains/Users/Pages/UsersNewPage.tsx
+++ packages/app/src/Domains/Users/Pages/UsersNewPage.tsx
@@ -1,11 +1,14 @@
 import AnimatedLayout from '@app/Aplication/Components/AnimatedLayout';
 import { NewUserForm } from '../Components';
 import { Page } from '@app/Aplication/Components/Page/Page';
+import { Card } from '@app/Aplication/Components/ui/card';
 
 export const UsersNewPage = () => (
-  <Page title="Nuevo usuario">
+  <Page title="Nuevo usuario" size="small">
     <AnimatedLayout>
-      <NewUserForm />
+      <Card className="p-4">
+        <NewUserForm />
+      </Card>
     </AnimatedLayout>
   </Page>
 );
