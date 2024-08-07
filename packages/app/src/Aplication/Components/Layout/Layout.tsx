a7c23a796d3e4567a4d66bc3529b42ae690561d8
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-07 10:27:30 -0300

    feat(app): improve layout

--- packages/app/src/Aplication/Components/Layout/Layout.tsx
+++ packages/app/src/Aplication/Components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { App } from '@app/App';
 import { NavBar } from '../NavBar/NavBar';
 import './Layout.css';
+import { Header } from '../Header/Header';
 
 export const Layout = () => {
   return (
     <div className="layout">
-      <header></header>
-      <NavBar />
+      <Header />
+      <NavBar className="is-desktop" />
       <main className="main">
         <App />
       </main>
