56445b2957e292168bdbc5645a8c87567c5351c4
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-07 10:29:12 -0300

    feat(app): improve component page

--- packages/app/src/Aplication/Components/Page/Page.tsx
+++ packages/app/src/Aplication/Components/Page/Page.tsx
@@ -1,13 +1,21 @@
+import clsx from 'clsx';
 import { Title } from '../Typography/Title';
 
 interface PageProps {
   children: React.ReactNode;
   title: string;
+  size?: 'small' | 'full';
 }
 
-export const Page = ({ children, title }: PageProps) => (
-  <div className="page p-4 flex flex-col gap-6 bg-slate-50 md:p-6 md:pt-10">
-    <Title variant="h1">{title}</Title>
-    <section className="overflow-auto h-[100dvh] pb-36">{children}</section>
-  </div>
-);
+export const Page = ({ children, title, size = 'full' }: PageProps) => {
+  const classContainer = clsx('w-full', {
+    'max-w-[600px] mx-auto': size === 'small',
+  });
+
+  return (
+    <div className="page h-[100%] p-4 grid grid-rows-[48px_auto] grid-cols-1 gap-6 max-w-[100vw] bg-gray-50 md:p-6 md:pt-10">
+      <Title variant="h1">{title}</Title>
+      <section className={classContainer}>{children}</section>
+    </div>
+  );
+};
