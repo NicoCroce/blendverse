64e3fda54852c29526465108b6b0ca3837e53aef
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-21 01:31:46 -0300

    feat(app): added new component Container

--- packages/app/src/Aplication/Components/Layout/Container.tsx
+++ packages/app/src/Aplication/Components/Layout/Container.tsx
@@ -1,7 +1,30 @@
+import clsx from 'clsx';
+
+type AlignValues = 'start' | 'end' | 'center' | 'strech';
+
 interface ContainerProps {
   children: React.ReactNode;
+  row?: boolean;
+  className?: string;
+  align?: AlignValues;
+  justify?: AlignValues | 'between' | 'evenly' | 'around';
 }
 
-export const Container = ({ children }: ContainerProps) => (
-  <div className="flex flex-col gap-4">{children}</div>
-);
+export const Container = ({
+  children,
+  className = '',
+  row = false,
+  align = 'strech',
+  justify = 'start',
+}: ContainerProps) => {
+  const _className = clsx(
+    'flex gap-4',
+    `items-${align} justify-${justify}`,
+    className,
+    {
+      'flex-row': row,
+      'flex-col': !row,
+    },
+  );
+  return <div className={_className}>{children}</div>;
+};
