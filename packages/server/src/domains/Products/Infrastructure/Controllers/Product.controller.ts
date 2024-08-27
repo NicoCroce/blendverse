27b308ef606c214f91685757c4997575ea206fbb
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-08-27 00:18:27 -0300

    feat(server): improve controller, all methods were converted to constants

--- packages/server/src/domains/Products/Infrastructure/Controllers/Product.controller.ts
+++ packages/server/src/domains/Products/Infrastructure/Controllers/Product.controller.ts
@@ -5,69 +5,63 @@ import z from 'zod';
 export class ProductController {
   constructor(private productService: ProductsService) {}
 
-  getProducts = () =>
-    procedure.query(async () => await this.productService.getllProducts());
+  getProducts = procedure.query(
+    async () => await this.productService.getllProducts(),
+  );
 
-  getProduct = () =>
-    procedure
-      .input(z.string().min(1, 'ID is required'))
-      .query(async ({ input }) => await this.productService.getProduct(input));
+  getProduct = procedure
+    .input(z.string().min(1, 'ID is required'))
+    .query(async ({ input }) => await this.productService.getProduct(input));
 
-  getStock = () =>
-    procedure
-      .input(z.string().min(1, 'ID id required'))
-      .query(async ({ input }) => await this.productService.getStock(input));
+  getStock = procedure
+    .input(z.string().min(1, 'ID id required'))
+    .query(async ({ input }) => await this.productService.getStock(input));
 
-  createProduct = () =>
-    procedure
-      .input(
-        z.object({
-          name: z.string(),
-          description: z.string(),
-          stock: z.number(),
-          price: z.number(),
-        }),
-      )
-      .mutation(
-        async ({ input: { name, description, stock, price } }) =>
-          await this.productService.createProduct(
-            name,
-            description,
-            stock,
-            price,
-          ),
-      );
+  createProduct = procedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        stock: z.number(),
+        price: z.number(),
+      }),
+    )
+    .mutation(
+      async ({ input: { name, description, stock, price } }) =>
+        await this.productService.createProduct(
+          name,
+          description,
+          stock,
+          price,
+        ),
+    );
 
-  deleteProduct = () =>
-    procedure
-      .input(z.string())
-      .mutation(
-        async ({ input: id }) => await this.productService.deleteProduct(id),
-      );
+  deleteProduct = procedure
+    .input(z.string())
+    .mutation(
+      async ({ input: id }) => await this.productService.deleteProduct(id),
+    );
 
-  updateStock = () =>
-    procedure
-      .input(
-        z.object({
-          id: z.string().min(1, 'ID Is required'),
-          stock: z.number().gte(0, 'Stock must be at least 0'),
-        }),
-      )
-      .mutation(
-        async ({ input: { id, stock } }) =>
-          await this.productService.updateStock({ id, stock }),
-      );
+  updateStock = procedure
+    .input(
+      z.object({
+        id: z.string().min(1, 'ID Is required'),
+        stock: z.number().gte(0, 'Stock must be at least 0'),
+      }),
+    )
+    .mutation(
+      async ({ input: { id, stock } }) =>
+        await this.productService.updateStock({ id, stock }),
+    );
 
-  getSomeInfoProduct = () =>
-    procedure
-      .input(
-        z.object({
-          productId: z.string().min(1, 'ID Requerido de tipo string'),
-          params: z.union([z.string(), z.array(z.string())]),
-        }),
-      )
-      .query(
-        async ({ input }) =>
-          await this.productService.getSomeInfoProduct(input),
-      );
+  getSomeInfoProduct = procedure
+    .input(
+      z.object({
+        productId: z.string().min(1, 'ID Requerido de tipo string'),
+        params: z.union([z.string(), z.array(z.string())]),
+      }),
+    )
+    .query(
+      async ({ input }) => await this.productService.getSomeInfoProduct(input),
+    );
 }
--- packages/server/src/domains/Users/Infrastructure/Controllers/User.controller.ts
+++ packages/server/src/domains/Users/Infrastructure/Controllers/User.controller.ts
@@ -5,25 +5,24 @@ import z from 'zod';
 export class UserController {
   constructor(private userService: UserService) {}
 
-  getAllUsers = () =>
-    procedure.query(async () => await this.userService.getAllUsers());
+  getAllUsers = procedure.query(
+    async () => await this.userService.getAllUsers(),
+  );
 
-  getUser = () =>
-    procedure
-      .input(z.string())
-      .query(async ({ input }) => await this.userService.getUser(input));
+  getUser = procedure
+    .input(z.string())
+    .query(async ({ input }) => await this.userService.getUser(input));
 
-  registerUser = () =>
-    procedure
-      .input(
-        z.object({
-          name: z.string(),
-          mail: z.string(),
-          password: z.string(),
-        }),
-      )
-      .mutation(
-        async ({ input: { mail, name, password } }) =>
-          await this.userService.registerUser(mail, name, password),
-      );
+  registerUser = procedure
+    .input(
+      z.object({
+        name: z.string(),
+        mail: z.string(),
+        password: z.string(),
+      }),
+    )
+    .mutation(
+      async ({ input: { mail, name, password } }) =>
+        await this.userService.registerUser(mail, name, password),
+    );
 }
