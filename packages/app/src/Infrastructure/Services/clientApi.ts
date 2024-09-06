2aa0f3d55f09c335c5d0c971ebd5faba09879f87
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-06 13:50:31 -0300

    feat(app): token is hardcoded

--- packages/app/src/Infrastructure/Services/clientApi.ts
+++ packages/app/src/Infrastructure/Services/clientApi.ts
@@ -7,6 +7,12 @@ export const trpcClientApi = TrpcApi.createClient({
   links: [
     httpLink({
       url: URL_SERVER + '/api',
+      headers() {
+        return {
+          Authorization:
+            ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJ1c2VyIjoibmljb0AxMjMuY29tIiwiaWF0IjoxNzI1MzM0ODM4LCJleHAiOjE3Mjc5MjY4Mzh9.t_6nlI9dnnG88iZCMsuLHPqV8Yd67fNyH8zmGNAHHxw',
+        };
+      },
     }),
   ],
 });
