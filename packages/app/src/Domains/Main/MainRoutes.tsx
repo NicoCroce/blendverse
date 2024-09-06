b58dc8b956c26918d26c1c5898078b6552cb6908
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-06 13:48:35 -0300

    feat(app): improve code

--- packages/app/src/Domains/Main/MainRoutes.tsx
+++ packages/app/src/Domains/Main/MainRoutes.tsx
@@ -1,6 +1,8 @@
 import { Route } from 'react-router-dom';
 import { MainPage } from './Pages/Main.page';
 
+export const MAIN_ROUTE = '/main';
+
 export const MainRouter = [
-  <Route key="main" path="/" element={<MainPage />} />,
+  <Route key="main" path={MAIN_ROUTE} element={<MainPage />} />,
 ];
