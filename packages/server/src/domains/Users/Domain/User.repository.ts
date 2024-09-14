535f58fe1fd1b3bb280e4aae94487a834c707408
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-14 09:44:49 -0300

    feat(sever): added interfaces for all layers

--- packages/server/src/domains/Users/Domain/User.repository.ts
+++ packages/server/src/domains/Users/Domain/User.repository.ts
@@ -1,8 +1,20 @@
+import { IRequestContext } from '@server/Application';
 import { User } from './User.entity';
 
+interface IGetUsers extends IRequestContext {}
+interface IRegisterUser extends IRequestContext {
+  user: User;
+}
+interface IGetUser extends IRequestContext {
+  id: string;
+}
+interface IValidateUser extends IRequestContext {
+  mail: string;
+}
+
 export interface UserRepository {
-  save(user: User): Promise<User>;
-  findUser(id: string): Promise<User | null>;
-  getUsers(): Promise<User[]>;
-  validateUser(mail: string): Promise<User | null>;
+  getUsers({ requestContext }: IGetUsers): Promise<User[]>;
+  registerUser({ user, requestContext }: IRegisterUser): Promise<User>;
+  getUser({ id, requestContext }: IGetUser): Promise<User | null>;
+  validateUser({ mail }: IValidateUser): Promise<User | null>;
 }
