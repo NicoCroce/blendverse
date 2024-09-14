f0faf9c93af52cbc70da70f6d8fb1364748b1114
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-14 09:45:03 -0300

    feat(server): adapt all Services for Object format

--- packages/server/src/domains/Users/Application/Users.service.ts
+++ packages/server/src/domains/Users/Application/Users.service.ts
@@ -1,5 +1,5 @@
 import { executeUseCase } from '@server/Application/Adapters/ExecuteUseCase';
-import { GetAllUsers, GetUser, RegisterUser, User } from '../Domain';
+import { GetUsers, GetUser, RegisterUser, User } from '../Domain';
 import { RequestContext } from '@server/Application';
 
 interface IregisterUser {
@@ -11,26 +11,35 @@ interface IregisterUser {
 
 export class UsersService {
   constructor(
-    private readonly _getAllUsers: GetAllUsers,
+    private readonly _getUsers: GetUsers,
     private readonly _getUser: GetUser,
     private readonly _registerUser: RegisterUser,
   ) {}
 
   async getAllUsers(requestContext: RequestContext): Promise<User[]> {
-    return await executeUseCase(this._getAllUsers, requestContext);
+    return await executeUseCase({ useCase: this._getUsers, requestContext });
   }
 
   async registerUser(
     input: IregisterUser,
     requestContext: RequestContext,
   ): Promise<User> {
-    return await executeUseCase(this._registerUser, input, requestContext, {
-      mail: input.mail,
-      name: input.name,
+    return await executeUseCase({
+      useCase: this._registerUser,
+      input,
+      requestContext,
+      inputLog: {
+        mail: input.mail,
+        name: input.name,
+      },
     });
   }
 
-  async getUser(id: string, requestContext: RequestContext): Promise<User> {
-    return await executeUseCase(this._getUser, id, requestContext);
+  async getUser(input: string, requestContext: RequestContext): Promise<User> {
+    return await executeUseCase({
+      useCase: this._getUser,
+      input,
+      requestContext,
+    });
   }
 }
