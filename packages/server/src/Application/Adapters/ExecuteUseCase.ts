a1b6f19e2a0add6bcdde758c3a4a3402078bd957
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-14 09:38:22 -0300

    feat(server): adapt ExecuteService for Object format

--- packages/server/src/Application/Adapters/ExecuteUseCase.ts
+++ packages/server/src/Application/Adapters/ExecuteUseCase.ts
@@ -3,31 +3,41 @@ import { RequestContext } from '../Entities';
 import { IUseCase } from '../Interfaces/IUSeCase';
 import { TRPCErrorAdapter } from './TRPCErrorAdapter';
 
+interface IExecuteUseCase<TOutput, TInput> {
+  useCase: IUseCase<TOutput, TInput>;
+  input?: TInput;
+  requestContext: RequestContext;
+  /** Use this if you don't want to show the whole input   */
+  inputLog?: unknown;
+}
+
 /**
  * Description placeholder
  *
  * @async
  * @template [TOutput=void]
  * @template [TInput=unknown]
- * @param {IUseCase<TOutput, TInput>} useCase
- * @param {?TInput} [input]
- * @param {?RequestContext} [requestContext]
- * @param {?unknown} [inputLog] Use this if you don't want to show the whole input
- * @returns {unknown}
+ * @param {IExecuteUseCase<TOutput, TInput>} param0
+ * @param {IUseCase<TOutput, TInput>} param0.useCase
+ * @param {TInput} param0.input
+ * @param {RequestContext} param0.requestContext
+ * @param {unknown} param0.inputLog Use this if you don't want to show the whole input in logger
+ * @returns {Promise<TOutput>}
  */
-export const executeUseCase = async <TOutput = void, TInput = unknown>(
-  useCase: IUseCase<TOutput, TInput>,
-  input?: TInput,
-  requestContext?: RequestContext,
-  /** Use this if you don't want to show the whole input   */
-  inputLog?: unknown,
-) => {
+
+export const executeUseCase = async <TOutput = void, TInput = unknown>({
+  useCase,
+  input,
+  requestContext,
+  /** Use this if you don't want to show the whole input in logger  */
+  inputLog,
+}: IExecuteUseCase<TOutput, TInput>): Promise<TOutput> => {
   const errorAdapter = new TRPCErrorAdapter();
   try {
     loggerContextInput(requestContext || {}, inputLog || input).info(
       `Execute usecase: ${useCase.constructor.name}`,
     );
-    return await useCase.execute(input, requestContext);
+    return await useCase.execute({ input, requestContext });
   } catch (error) {
     throw errorAdapter.adapt(error, requestContext);
   }
