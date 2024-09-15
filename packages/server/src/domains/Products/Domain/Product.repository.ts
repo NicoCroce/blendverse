7af871cb855e328a622007be128cc532353faec5
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-15 11:30:05 -0300

    feat(server): updated products repository

--- packages/server/src/domains/Products/Domain/Product.repository.ts
+++ packages/server/src/domains/Products/Domain/Product.repository.ts
@@ -1,13 +1,40 @@
+import { IRequestContext } from '@server/Application';
 import { Product } from './Product.entity';
 
+export interface IGetProductsRepository extends IRequestContext {}
+export interface ICreateProductRepository extends IRequestContext {
+  product: Product;
+}
+export interface IUpdateStockRepository extends IRequestContext {
+  id: string;
+  stock: number;
+}
+export interface IDeleteProductRepository extends IRequestContext {
+  id: string;
+}
+export interface IGetProductRepository extends IRequestContext {
+  id: string;
+}
+export interface IGetProductInfoRepository extends IRequestContext {
+  productId: string;
+  params: string | string[];
+}
+
 export interface ProductsRepository {
-  getProducts(): Promise<Product[]>;
-  create(product: Product): Promise<Product>;
-  updateStock(id: string, stock: number): Promise<Product>;
-  delete(id: string): Promise<Product>;
-  getProduct(id: string): Promise<Product | null>;
-  getProductInfo(
-    productId: string,
-    params: string | string[],
-  ): Promise<Record<string, unknown> | null>;
+  getProducts({ requestContext }: IGetProductsRepository): Promise<Product[]>;
+  createProduct({
+    product,
+    requestContext,
+  }: ICreateProductRepository): Promise<Product>;
+  updateStock({
+    id,
+    stock,
+    requestContext,
+  }: IUpdateStockRepository): Promise<Product>;
+  deleteProduct({ id }: IDeleteProductRepository): Promise<Product>;
+  getProduct({ id }: IGetProductRepository): Promise<Product | null>;
+  getProductInfo({
+    productId,
+    params,
+  }: IGetProductInfoRepository): Promise<Record<string, unknown> | null>;
 }
