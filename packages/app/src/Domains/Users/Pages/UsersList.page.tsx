ccf8232b3457dd6679717de95994f7a3d78b7dcc
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-20 00:28:57 -0300

    feat(app): improve style and add type

--- packages/app/src/Domains/Users/Pages/UsersList.page.tsx
+++ packages/app/src/Domains/Users/Pages/UsersList.page.tsx
@@ -5,7 +5,7 @@ export const UsersListPage = () => {
   return (
     <Page title="Página de Usuarios">
       <div className="flex flex-col items-start gap-2">
-        <div className="w-full flex justify-between">
+        <div className="w-full flex justify-between gap-4">
           <SearchUser />
           <NewUserButton />
         </div>
