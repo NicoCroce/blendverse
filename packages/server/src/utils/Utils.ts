f1fe45c234ddcba1e4e8e9a906f7659c756ab183
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-23 18:30:41 -0300

    feat(server): improve delay service

--- packages/server/src/utils/Utils.ts
+++ packages/server/src/utils/Utils.ts
@@ -1,4 +1,4 @@
-export const delay = (time: number = 10) => {
+export const delay = (time: number = 300) => {
   const randomDelay = Math.floor(Math.random() * 3) + 1;
   return new Promise((resolve) => setTimeout(resolve, randomDelay * time));
 };
