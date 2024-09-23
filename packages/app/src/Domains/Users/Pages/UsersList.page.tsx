b2faf37be6289013118da8274cbd65b974c55112
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-23 18:31:09 -0300

    feat(app): improve Page component. Now accept headerRight component

--- packages/app/src/Domains/Users/Pages/UsersList.page.tsx
+++ packages/app/src/Domains/Users/Pages/UsersList.page.tsx
@@ -1,18 +1,9 @@
-import { Container } from '@app/Aplication';
 import { UsersList, SearchUser, NewUserButton } from '../Components';
 import { Page } from '@app/Aplication/Components/Layout/Page';
 
 export const UsersListPage = () => {
   return (
-    <Page
-      title="Página de Usuarios"
-      headerRight={
-        <Container row>
-          <NewUserButton />
-          <NewUserButton />
-        </Container>
-      }
-    >
+    <Page title="Página de Usuarios" headerRight={<NewUserButton />}>
       <div className="flex flex-col items-start gap-2">
         <SearchUser />
         <UsersList />
