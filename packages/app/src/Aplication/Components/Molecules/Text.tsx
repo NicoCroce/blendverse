d001a3a74692b350b7d18171f847cef5da8b671a
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-10-03 18:15:50 -0300

    feat(app): componentes - improve texts

--- packages/app/src/Aplication/Components/Molecules/Text.tsx
+++ packages/app/src/Aplication/Components/Molecules/Text.tsx
@@ -2,32 +2,39 @@ import React from 'react';
 
 interface TextProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const Text = ({ children }: TextProps) => (
-  <p className="leading-7">{children}</p>
+export const Text = ({ children, className = '' }: TextProps) => (
+  <p className={`leading-7 ${className}`}>{children}</p>
 );
 
-const TextBlockquote = ({ children }: TextProps) => (
-  <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>
+const TextBlockquote = ({ children, className = '' }: TextProps) => (
+  <blockquote className={`mt-6 border-l-2 pl-6 italic ${className}`}>
+    {children}
+  </blockquote>
 );
 
-const TextCode = ({ children }: TextProps) => (
-  <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+const TextCode = ({ children, className = '' }: TextProps) => (
+  <code
+    className={`relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold ${className}`}
+  >
     {children}
   </code>
 );
 
-const TextLead = ({ children }: TextProps) => (
-  <p className="text-xl text-muted-foreground">{children}</p>
+const TextLead = ({ children, className = '' }: TextProps) => (
+  <p className={`text-xl text-muted-foreground ${className}`}>{children}</p>
 );
 
-const TextSmall = ({ children }: TextProps) => (
-  <small className="text-sm font-medium leading-6">{children}</small>
+const TextSmall = ({ children, className = '' }: TextProps) => (
+  <small className={`text-sm font-medium leading-6 ${className}`}>
+    {children}
+  </small>
 );
 
-const TextMuted = ({ children }: TextProps) => (
-  <p className="text-sm text-muted-foreground">{children}</p>
+const TextMuted = ({ children, className = '' }: TextProps) => (
+  <p className={`text-sm text-muted-foreground ${className}`}>{children}</p>
 );
 
 Text.Blockquote = TextBlockquote;
