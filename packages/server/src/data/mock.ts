f8f6cd1eafee9a7d7176598bcee422c02cbb7885
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-10-03 08:39:53 -0300

    feat(server): update company image in mock file

--- packages/server/src/data/mock.ts
+++ packages/server/src/data/mock.ts
@@ -5,7 +5,7 @@ export const Users = [
     mail: 'admin@admin.com',
     password: '$2a$10$pxMW7.kXzU9YTOTVj4YSh.rN/ijGSOA8T6vbbSXR/Eu3wGa55aVUm',
     userImage:
-      'https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png',
+      'https://media.licdn.com/dms/image/v2/D4D03AQGnZHqP2ZrCpQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1707777018397?e=1733356800&v=beta&t=j1UYX7BCmIey_cdb86UTLNpXPbmMEcrlDeweWqDV_FY',
     companyLogo:
       'https://media.licdn.com/dms/image/v2/C560EAQFi2OGWfxI4qA/rightRail-logo-shrink_200_200/rightRail-logo-shrink_200_200/0/1630998002580?e=1728522000&v=beta&t=SlHmEK_Z3rKJWOcqgW_aH2BPuj_c33yarTSrjr6gu6s',
     companyName: 'LinkedIn',
@@ -16,7 +16,7 @@ export const Users = [
     mail: 'prueba@gmail.com',
     password: '$2a$10$pxMW7.kXzU9YTOTVj4YSh.rN/ijGSOA8T6vbbSXR/Eu3wGa55aVUm',
     userImage:
-      'https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png',
+      'https://media.licdn.com/dms/image/v2/D4D03AQGnZHqP2ZrCpQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1707777018397?e=1733356800&v=beta&t=j1UYX7BCmIey_cdb86UTLNpXPbmMEcrlDeweWqDV_FY',
     companyLogo:
       'https://media.licdn.com/dms/image/v2/C560EAQFi2OGWfxI4qA/rightRail-logo-shrink_200_200/rightRail-logo-shrink_200_200/0/1630998002580?e=1728522000&v=beta&t=SlHmEK_Z3rKJWOcqgW_aH2BPuj_c33yarTSrjr6gu6s',
     companyName: 'LinkedIn',
