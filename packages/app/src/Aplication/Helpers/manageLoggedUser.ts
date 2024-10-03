a8885dfb5d87eadb1895af50fb953f827c83e747
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-10-03 08:48:33 -0300

    feat(app): delete user when app logout

--- packages/app/src/Aplication/Helpers/manageLoggedUser.ts
+++ packages/app/src/Aplication/Helpers/manageLoggedUser.ts
@@ -4,6 +4,10 @@ export const setLoggedUser = <T>(data: T) => {
   return localStorage.setItem('dataUser', JSON.stringify(data));
 };
 
+export const removeLoggedUser = () => {
+  return localStorage.removeItem('dataUser');
+};
+
 export const getLoggedUser = () => {
   try {
     const savedUserData = localStorage.getItem('dataUser');
