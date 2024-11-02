ced92196c9e3e629b9eb1e1976ea09bd0d39d7b6
Nico Croce <ingcrocenicolas@outlook.com>
2024-11-02 10:45:47 -0300

    feat(server): added useCase and validations

---
+++ packages/server/src/domains/Users/Domain/UseCases/ChangePassword.usecase.ts
@@ -0,0 +1,36 @@
+import { AppError, executeUseCase, IUseCase } from '@server/Application';
+import { ValidateUserPassword } from '@server/domains/Auth';
+import { IChangePassword } from '../User.interfaces';
+import { UserRepository } from '../User.repository';
+
+export class ChangePassword implements IUseCase<void> {
+  constructor(
+    private readonly usersRepository: UserRepository,
+    private readonly _validateUserPassword: ValidateUserPassword,
+  ) {}
+
+  async execute({
+    input: { password, newPassword, rePassword },
+    requestContext,
+  }: IChangePassword): Promise<void> {
+    if (newPassword !== rePassword) {
+      throw new AppError('Las contraseñas nuevas no coinciden');
+    }
+    const user = await executeUseCase({
+      useCase: this._validateUserPassword,
+      input: { id: requestContext.values.userId, password },
+      requestContext,
+    });
+
+    if (!user) throw new AppError('No se puede validar el usuario');
+
+    try {
+      await this.usersRepository.changePassword({
+        password,
+        requestContext,
+      });
+    } catch (error) {
+      throw new AppError('No se pudo cambiar la constraseña');
+    }
+  }
+}
