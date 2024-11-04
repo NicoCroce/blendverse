d556b390cba177e19049d99178c1533470ed36ab
Nico Croce <ingcrocenicolas@outlook.com>
2024-11-04 17:31:26 -0300

    feat(app): added new Modal component

--- packages/app/src/Aplication/Components/Molecules/index.ts
+++ packages/app/src/Aplication/Components/Molecules/index.ts
@@ -3,6 +3,7 @@ export * from './Button';
 export * from './Input';
 export * from './Link';
 export * from './List';
+export * from './Modal';
 export * from './OutletSheet';
 export * from './Text';
 export * from './Title';
