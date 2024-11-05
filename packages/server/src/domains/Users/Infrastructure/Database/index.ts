58de10e67efe68eb43a1a0fde4898b6d3cf812a5
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-11-05 09:23:35 -0300

    feat: macrosistemas change password implementation (#20)
    * feat(server): update scheme with renovar_clave
    
    * feat(server): implement changePassword method and update database
    
    * feat(server): delete unnecessary files and functionalities

--- packages/server/src/domains/Users/Infrastructure/Database/index.ts
+++ packages/server/src/domains/Users/Infrastructure/Database/index.ts
@@ -1,5 +1,3 @@
-export * from './Local.database';
 export * from './Relations';
 export * from './Users.scheme';
-export * from './UsersRepository.implementation.localDB';
 export * from './UsersRepository.implementation';
