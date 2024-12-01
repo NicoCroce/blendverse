60688f4b3b5dec35141b6a5441f2df634897755b
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-01 11:31:15 -0300

    feat(server): added interfaces and type for permissions and roles

--- packages/server/src/domains/Permissions/Domain/Permissions.repository.ts
+++ packages/server/src/domains/Permissions/Domain/Permissions.repository.ts
@@ -5,6 +5,13 @@ import { Roles } from './Roles.entity';
 export interface IGetPermissionsRepository extends IRequestContext {}
 export interface IGetRolesRepository extends IRequestContext {}
 export interface IGetPermissionsByUserRepository extends IRequestContext {}
+export interface IAssociateUserToRoleRepository extends IRequestContext {
+  userId: number;
+  role: string;
+}
+export interface IGetRoleByUserRepository extends IRequestContext {
+  userId: number;
+}
 
 export interface PermissionsRepository {
   getPermissions(params: IGetPermissionsRepository): Promise<Permissions[]>;
@@ -12,4 +19,6 @@ export interface PermissionsRepository {
   getPermissionsByUser(
     params: IGetPermissionsByUserRepository,
   ): Promise<string[]>;
+  associateUserToRole(params: IAssociateUserToRoleRepository): Promise<void>;
+  getRoleByUser(params: IGetRoleByUserRepository): Promise<string | null>;
 }
