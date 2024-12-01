08fec189d78a714717eb1718d186410f7cf3f3c0
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-01 11:17:01 -0300

    feat(server): added relation between roles and users

--- packages/server/src/data/mock.ts
+++ packages/server/src/data/mock.ts
@@ -10,7 +10,6 @@ export const Users = [
     companyLogo:
       'https://www.colorpluspinturerias.com/assets/images/color-plus-logo.png',
     companyName: 'Color Plus',
-    rol: 'recurso-humano',
   },
   {
     id: 2,
@@ -23,7 +22,6 @@ export const Users = [
     companyLogo:
       'https://media.licdn.com/dms/image/v2/C560EAQFi2OGWfxI4qA/rightRail-logo-shrink_200_200/rightRail-logo-shrink_200_200/0/1630998002580?e=1728522000&v=beta&t=SlHmEK_Z3rKJWOcqgW_aH2BPuj_c33yarTSrjr6gu6s',
     companyName: 'LinkedIn',
-    rol: null,
   },
 ];
 
@@ -70,12 +68,29 @@ export const Permissions = [
     name: 'dasdboard-access',
     description: 'Lee el estado de todos los documentos',
   },
+  {
+    name: 'users-access',
+    description:
+      'Accede al listado de todos lo susuarios con la posibilidad de agregar, editar y eliminar',
+  },
 ];
 
 export const Roles = [
   {
+    name: 'admin-access',
+    description: 'Tienen acceso a toda la información de los empleados',
+    permissions: ['dashboard-access', 'users-access'],
+  },
+  {
     name: 'recurso-humano',
     description: 'Tienen acceso a toda la información de los empleados',
     permissions: ['dashboard-access'],
+  },
+];
+
+export const Roles_Users = [
+  {
+    userId: 1,
+    roleName: 'admin-access',
   },
 ];
