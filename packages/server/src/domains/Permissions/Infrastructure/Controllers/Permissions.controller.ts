553cd154293408c43b959040991a69268d230c26
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-01 11:32:46 -0300

    feat(server): added controller to get data by service

--- packages/server/src/domains/Permissions/Infrastructure/Controllers/Permissions.controller.ts
+++ packages/server/src/domains/Permissions/Infrastructure/Controllers/Permissions.controller.ts
@@ -1,6 +1,7 @@
 import { protectedProcedure } from '@server/Infrastructure';
 import { PermissionsService } from '../../Aplication';
 import { executeService } from '@server/Application';
+import z from 'zod';
 
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
@@ -24,4 +25,12 @@ export class PermissionsController {
       ),
     ),
   );
+
+  getRoleByUser = protectedProcedure
+    .input(z.number())
+    .query(
+      executeService(
+        this.permissionsService.getRoleByUser.bind(this.permissionsService),
+      ),
+    );
 }
