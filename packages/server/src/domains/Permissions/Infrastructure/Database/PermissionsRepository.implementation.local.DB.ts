fff77cee071fad953d4a725c554f87fac1936ce5
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-01 11:34:27 -0300

    feat(server): implement methods to associete role and get rol by user in repository

--- packages/server/src/domains/Permissions/Infrastructure/Database/PermissionsRepository.implementation.local.DB.ts
+++ packages/server/src/domains/Permissions/Infrastructure/Database/PermissionsRepository.implementation.local.DB.ts
@@ -1,6 +1,8 @@
 import {
+  IAssociateUserToRoleRepository,
   IGetPermissionsByUserRepository,
   IGetPermissionsRepository,
+  IGetRoleByUserRepository,
   IGetRolesRepository,
   Permissions,
   PermissionsRepository,
@@ -35,5 +37,22 @@ export class PermissionsRepositoryImplementationLocal
     return (
       (await this.Db.getPermissionsByUser(requestContext.values.userId)) || ['']
     );
+  }
+
+  async associateUserToRole({
+    userId,
+    role,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    requestContext,
+  }: IAssociateUserToRoleRepository): Promise<void> {
+    return await this.Db.associateUserToRole(userId, role);
+  }
+
+  async getRoleByUser({
+    userId,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    requestContext,
+  }: IGetRoleByUserRepository): Promise<string | null> {
+    return this.Db.getRoleByUser(userId);
   }
 }
