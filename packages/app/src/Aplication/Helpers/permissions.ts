f3f1badc5044e252f27cd6076ce785121ba3d2e4
Nico Croce <ingcrocenicolas@outlook.com>
2025-04-12 19:43:55 -0300

    feat(app): update all dependencies and routes and permissions

--- packages/app/src/Aplication/Helpers/permissions.ts
+++ packages/app/src/Aplication/Helpers/permissions.ts
@@ -1,2 +1,2 @@
 export const DASHBOARD_ACCESS = 'dashboard-access';
-export const USER_ACCESS = 'admin-users';
+export const USER_ACCESS = 'users-access';
--- packages/app/src/Domains/Auth/Auth.routes.tsx
+++ packages/app/src/Domains/Auth/Auth.routes.tsx
@@ -1,2 +1,3 @@
 export const AUTH_ROUTE = '/';
 export const RESTORE_PASSWORD = '/reset-password';
+export const CHANGE_PASSWORD_PUBLIC = '/change-password';
