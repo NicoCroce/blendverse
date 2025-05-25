423eeba378be4fa1d4564fc03a5a0dec62166566
Nico Croce <ingcrocenicolas@outlook.com>
2025-05-25 20:11:02 -0300

    feat(server): return all streets without filter

--- packages/server/src/domains/Streets/Infrastructure/Database/Local.database.ts
+++ packages/server/src/domains/Streets/Infrastructure/Database/Local.database.ts
@@ -5,6 +5,8 @@ export class LocalDatabaseStreets {
   getAll = async (filters: string | undefined) => {
     await delay();
 
+    if (!filters) return Streets;
+
     return Streets.filter(({ denominacion }) =>
       denominacion.toLowerCase().includes((filters || '').toLowerCase()),
     ).slice(0, 10);
