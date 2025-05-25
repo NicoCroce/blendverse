ce69350c3ad1068e41b6a51132615430381e4ed9
Nico Croce <ingcrocenicolas@outlook.com>
2025-05-25 00:36:52 -0300

    feat(app): added SelectStreet component. Use this as example to reply others

---
+++ packages/app/src/Aplication/Components/Molecules/Selects/SelectStreet.tsx
@@ -0,0 +1,29 @@
+import { FieldValues, Path, UseFormReturn } from 'react-hook-form';
+import { useState } from 'react';
+import { SelectBase } from './SelectBase';
+import { useGetStreets } from './Hooks/useGetStreets';
+
+interface SelectStreetProps<T extends FieldValues> {
+  name: Path<T>;
+  form: UseFormReturn<T>;
+}
+
+export const SelectStreet = <T extends FieldValues>({
+  name,
+  form,
+}: SelectStreetProps<T>) => {
+  const [search, setSearch] = useState('');
+  const { data: StreetOptions } = useGetStreets(search);
+
+  const handleChangeFilter = (value: string) => setSearch(value);
+
+  return (
+    <SelectBase
+      form={form}
+      name={name}
+      inputLabel="Calle"
+      options={StreetOptions}
+      onChangeFilter={handleChangeFilter}
+    />
+  );
+};
