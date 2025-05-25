6eee8f508882fe4bdf153dc847b32ff11005445e
Nico Croce <ingcrocenicolas@outlook.com>
2025-05-25 01:21:53 -0300

    feat(app): added Spinner component

---
+++ packages/app/src/Aplication/Components/Molecules/Spinner.tsx
@@ -0,0 +1,6 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Spinner = () => (
+  <FontAwesomeIcon icon={faSpinner} spin size="1x" />
+);
