21cd759600c88dba18c4f595157e2df3df51a4ab
Nico Croce <ingcrocenicolas@outlook.com>
2025-06-14 08:38:40 -0300

    feat(server): added role in User entity

--- packages/server/src/domains/Users/Infrastructure/Database/Relations.ts
+++ packages/server/src/domains/Users/Infrastructure/Database/Relations.ts
@@ -1,5 +1,7 @@
 import { CompaniesModel } from '@server/domains/Companies/Infrastructure';
 import { UserModel } from './Users.model';
 
-CompaniesModel.hasMany(UserModel, { foreignKey: 'id_propietario' });
-UserModel.belongsTo(CompaniesModel, { foreignKey: 'id_propietario' });
+export const relateUsers = () => {
+  CompaniesModel.hasMany(UserModel, { foreignKey: 'id_propietario' });
+  UserModel.belongsTo(CompaniesModel, { foreignKey: 'id_propietario' });
+};
