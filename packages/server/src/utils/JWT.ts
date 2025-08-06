03ba86aac5fd1d32eb54fc0d708a2c2896404f9c
Nico Croce <ingcrocenicolas@outlook.com>
2025-08-05 23:28:45 -0300

    feat(server): improve import in JWT

--- packages/server/src/utils/JWT.ts
+++ packages/server/src/utils/JWT.ts
@@ -1,18 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { logger } from './pino';
 
-const secretKey = process.env.SECRET_KEY_BACK || '';
-
-if (!secretKey) throw new Error('You must specify SECRET_KEY_BACK in env file');
+const getSecretKey = () => {
+  const secretKey = process.env.SECRET_KEY_BACK || '';
+  if (!secretKey)
+    throw new Error('You must specify SECRET_KEY_BACK in env file');
+  return secretKey;
+};
 
 export const generateToken = (data: object, expire: string = '30D') =>
-  jwt.sign(data, secretKey, { expiresIn: expire });
+  jwt.sign(data, getSecretKey(), { expiresIn: expire });
 
 export const verifyToken = (
   token: string,
 ): Promise<string | jwt.JwtPayload | undefined> =>
   new Promise((resolve, reject) => {
-    jwt.verify(token, secretKey, (err, decoded) => {
+    jwt.verify(token, getSecretKey(), (err, decoded) => {
       if (err) {
         logger.error(`Token not provided ${err}} - TOKEN: ${token}}`);
         return reject(err);
