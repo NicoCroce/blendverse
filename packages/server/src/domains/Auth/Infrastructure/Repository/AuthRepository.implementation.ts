ed02290601ec56147510f91da4016fca68f063d9
Nico Croce <ingcrocenicolas@outlook.com>
2026-04-19 20:46:30 -0300

    refactor(AuthRepository): extraer constante para el endpoint de restauración de contraseña

--- packages/server/src/domains/Auth/Infrastructure/Repository/AuthRepository.implementation.ts
+++ packages/server/src/domains/Auth/Infrastructure/Repository/AuthRepository.implementation.ts
@@ -1,8 +1,10 @@
 import { axios } from '@server/utils/axios';
 import { AuthRepository, IRestorePasswordRepository } from '../../Domain';
 
+const RESTORE_PASSWORD_ENDPOINT = '/nuevousuario/sendReestablecer';
+
 export class AuthRepositoryImplementation implements AuthRepository {
   async restorePassword({ mail }: IRestorePasswordRepository): Promise<void> {
-    await axios.post('/nuevousuario/sendReestablecer', { email: mail });
+    await axios.post(RESTORE_PASSWORD_ENDPOINT, { email: mail });
   }
 }
