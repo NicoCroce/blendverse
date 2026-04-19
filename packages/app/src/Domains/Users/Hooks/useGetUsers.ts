8dc0782e5acba9efcaf37dbdf1e3b18b58abafe4
Nico Croce <ingcrocenicolas@outlook.com>
2026-04-19 20:40:16 -0300

    feat(useGetUsers): agregar opciones de refetch y tiempo de stale para la consulta de usuarios

--- packages/app/src/Domains/Users/Hooks/useGetUsers.ts
+++ packages/app/src/Domains/Users/Hooks/useGetUsers.ts
@@ -5,6 +5,9 @@ import { UsersService } from '../Users.service';
 export const useGetUsers = () => {
   const { searchParams } = useURLParams<TUserSearch>();
   return UsersService.getAll.useQuery(searchParams, {
+    staleTime: 1000,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
     placeholderData: (prev) => prev,
   });
 };
